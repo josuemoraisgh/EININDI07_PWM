290d4fdac725414e3f7eeef955301dcb1124075e
josuemoraisgh <josuemorais@gmail.com>
2024-08-30 10:27:54 -0300

    refactor main

--- src/main0.cpp
+++ src/main0.cpp
@@ -3,10 +3,10 @@
 
 #define pinPWM 6                            //Configura o pino de Saida do PWM
 #define PERIODO1 100                        //Em us 100ms
-#define PERIODO2 16                         //Em us 100ms
+#define PERIODO2 16                         //Em us 16ms
+#define TIME_DELAY_MS 1
 
-uint32_t timeDelayMS = 1;
-uint32_t expiresDelayMS = 0;
+uint64_t previousTimeMS = 0;
 byte count1 = 0;
 byte count2 = 0;
 
@@ -18,11 +18,12 @@ void setup() // Codigo de configuração
 
 void loop()
 {
-  if ((millis() - expiresDelayMS) >= timeDelayMS)
+  const uint64_t currentTimeMS = millis();
+  if ((currentTimeMS - previousTimeMS) >= TIME_DELAY_MS)
   {
-    expiresDelayMS = millis();
+    previousTimeMS = currentTimeMS;
     //analogWrite(pinPWM,100*sin(2*PI*count1/PERIODO1)+27*sin(2*PI*count2/PERIODO2)+127);
-    analogWrite(pinPWM, 127 * sin(2 * PI * count1 / PERIODO1) + 127.5);
+    analogWrite(pinPWM, 127 * sin(2 * PI * count1 / PERIODO1) + 127);
     if (++count1 >= PERIODO1) count1 = 0;
     if (++count2 >= PERIODO2) count2 = 0;    
   }
