d788ec3157fe3b78d302d7e2dcda18442a7a3783
josuemoraisgh <josuemorais@gmail.com>
2024-09-10 10:33:43 -0300

    refactor esp4

--- src/esp4.cpp
+++ src/esp4.cpp
@@ -4,34 +4,46 @@
 #define FREQ1 110.0
 #define FREQ2 10.0
 
-#define TIME_DELAY 100.0 // Time em milisegundos
+#define TIME_DELAY1 100.0 // Time em microsegundos
 
-#define CILCE_FREQ1 (FREQ1 * TIME_DELAY / 1000000.0)
+#define CILCE_FREQ1 (FREQ1 * TIME_DELAY1 / 1000000.0)
 #define CILCE_PERIODO1 (1.0 / CILCE_FREQ1)
-#define CILCE_FREQ2 (FREQ2 * TIME_DELAY / 1000000.0)
+#define CILCE_FREQ2 (FREQ2 * TIME_DELAY1 / 1000000.0)
 #define CILCE_PERIODO2 (1.0 / CILCE_FREQ2)
 
-xQueueHandle interputQueue = xQueueCreate(10, sizeof(uint16_t *));
-
-void plotWave(void *)
+void writeWave(void *)
 {
-  uint16_t timeWave1 = 0;                          // Índice para percorrer a tabela de formas de onda
-  uint16_t timeWave2 = 0;   
-  AsyncDelay_c delayPlotWave(TIME_DELAY, ISMICROS); // time in micro second
+  uint16_t timeWave1 = 0; // Índice para percorrer a tabela de formas de onda
+  uint16_t timeWave2 = 0;
+  AsyncDelay_c delayWriteWave(TIME_DELAY1, ISMICROS); // time in micro second
   for (;;)
   {
-    if (delayPlotWave.isExpired())
+    if (delayWriteWave.isExpired())
     {
-      delayPlotWave.repeat();
-      const double aux = 1.0 + 0.3*sin(2.0 * PI * CILCE_FREQ1 * timeWave1) + 0.7*sin(2.0 * PI * CILCE_FREQ2 * timeWave2);
-      ledcWrite(0, uint16_t(511.0*aux));
-      dacWrite(def_pin_DAC1, uint8_t(127.0*aux));
-      
-      const uint16_t value = analogRead(def_pin_ADC1);
-      xQueueSend(interputQueue, &value, 0);
-      
-      if (++timeWave1 >= CILCE_PERIODO1) timeWave1 = 0;
-      if (++timeWave2 >= CILCE_PERIODO2) timeWave2 = 0;      
+      delayWriteWave.repeat();
+      const double aux = 1.0 + 0.3 * sin(2.0 * PI * CILCE_FREQ1 * timeWave1) + 0.7 * sin(2.0 * PI * CILCE_FREQ2 * timeWave2);
+      ledcWrite(0, uint16_t(511.0 * aux));
+      dacWrite(def_pin_DAC1, uint8_t(127.0 * aux));
+
+      if (++timeWave1 >= CILCE_PERIODO1)
+        timeWave1 = 0;
+      if (++timeWave2 >= CILCE_PERIODO2)
+        timeWave2 = 0;
+    }
+  }
+}
+
+
+#define TIME_DELAY2 1.0 // Time em milisegundos
+void readWave(void *)
+{
+  AsyncDelay_c delayReadWave(TIME_DELAY2, ISMILI); // time in mili second  
+  for (;;)
+  {
+    if (delayReadWave.isExpired())
+    {
+      delayReadWave.repeat();
+      IIKit.WSerial.plot("Data", analogRead(def_pin_ADC1));
     }
   }
 }
@@ -39,19 +51,13 @@ void plotWave(void *)
 void setup()
 {
   IIKit.setup();
-  ledcAttachPin(def_pin_PWM,0);
+  ledcAttachPin(def_pin_PWM, 0);
   ledcSetup(0, 1000, 10);
-  xTaskCreate(plotWave,"Task Wave",5000,NULL,1,NULL);
+  xTaskCreate(writeWave, "Task Wave", 5000, NULL, 1, NULL);
+  xTaskCreate(readWave, "Task Wave", 5000, NULL, 1, NULL);  
 }
-
-uint32_t count = 0;
-uint16_t pvBuffer = 0;
 
 void loop()
 {
   IIKit.loop();
-  if(xQueueReceive(interputQueue, &pvBuffer, 0))
-  {
-    IIKit.WSerial.plot("Data",count++, pvBuffer);
-  }  
 }
