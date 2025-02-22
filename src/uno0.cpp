4d5c8701f5bedcea063635840d9466679a61a829
josuemorais <josuemorais@gmail.com>
2025-02-22 06:12:03 -0300

    refactor uno0 and uno1

--- src/uno0.cpp
+++ src/uno0.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <math.h>
 
-#define pinPWM 6                            //Configura o pino de Saida do PWM
-#define PERIODO1 100                        //Em us 100ms
+#define pinPWM 6      //Configura o pino de Saida do PWM
+#define PERIODO 100   //Em ms 100ms
 
-byte count1 = 0;
+byte count = 0;
 
 void setup() // Codigo de configuração
 {
@@ -12,15 +12,15 @@ void setup() // Codigo de configuração
   pinMode(pinPWM, OUTPUT);
 }
 
-#define TIME_DELAY_MS 1
+#define TIME_RESOLUTION 1
 uint32_t previousTimeMS = 0;
 void loop()
 {
   const uint32_t currentTimeMS = millis();
-  if ((currentTimeMS - previousTimeMS) >= TIME_DELAY_MS)
+  if ((currentTimeMS - previousTimeMS) >= TIME_RESOLUTION)
   {
     previousTimeMS = currentTimeMS;
-    analogWrite(pinPWM, 127 * sin(2 * PI * count1 / PERIODO1) + 127);
-    if (++count1 >= PERIODO1) count1 = 0;    
+    analogWrite(pinPWM, 127 * sin(2 * PI * count / PERIODO) + 127);
+    count = (count + 1) % PERIODO;     
   }
 }
